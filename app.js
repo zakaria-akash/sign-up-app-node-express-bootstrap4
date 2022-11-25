d5de80caaeb97f21d1c3475c70841fe9c36162d9
zakaria-akash <zakaria.93@yahoo.com>
2022-11-25 08:23:52 +0600

    initializing the app with all necessary files and packages; adding initial commands to app.js file to run the server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000 ...");
+});
