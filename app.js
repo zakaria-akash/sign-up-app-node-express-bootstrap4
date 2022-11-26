1b7cd9711b6eb6c9e28086ac9963a33245fecd58
zakaria-akash <zakaria.93@yahoo.com>
2022-11-26 09:08:54 +0600

    finishing the initial setup for sign-up page by adding a post method to the home route

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", (req, res) => {
+  let firstName = req.body.fName;
+  let lastName = req.body.lName;
+  let email = req.body.email;
+
+  console.log("First Name: " + firstName);
+  console.log("Last Name: " + lastName);
+  console.log("Email: " + email);
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000 ...");
 });
