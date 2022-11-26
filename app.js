3f5e95500db4c62aea1589ff573e474fac9f9993
zakaria-akash <zakaria.93@yahoo.com>
2022-11-26 07:34:48 +0600

    adding custom bootstrap 4 sign up page with my photo; adding styles for sign-up page

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000 ...");
 });
